9337b76a65181812b9de7a47160c475eb4e2d252
Calvin Neo <calvinneo1995@gmail.com>
2024-10-12 15:12:50 +0800

    add more logs
    Signed-off-by: Calvin Neo <calvinneo1995@gmail.com>

--- dbms/src/Storages/Page/V3/Universal/S3PageReader.cpp
+++ dbms/src/Storages/Page/V3/Universal/S3PageReader.cpp
@@ -41,12 +41,15 @@ std::tuple<Page, S3::S3RandomAccessFilePtr> S3PageReader::readWithS3File(const U
     S3::S3RandomAccessFilePtr s3_remote_file;
     auto s3_client = S3::ClientFactory::instance().sharedTiFlashClient();
     if (file == nullptr || location.offset_in_file <= file->getPos() || remote_name != file->getRemoteFileName()) {
-        if (file != nullptr && location.offset_in_file <= file->getPos()) {
-            LOG_DEBUG(DB::Logger::get(), "!!!!! read befored1! {} {} {} location.size_in_file {}", location.offset_in_file, file->getPos(), file->getPrefetchedSize(), location.size_in_file);
+        if (file == nullptr) {
+            LOG_DEBUG(DB::Logger::get(), "!!!!! read befored1.1! {} {} {} location.size_in_file {} remote_name={}", location.offset_in_file, file->getPos(), file->getPrefetchedSize(), location.size_in_file, remote_name);
             ProfileEvents::increment(ProfileEvents::S3PageReaderNotReusedFile1, 1);
         }
         else
+        {
+            LOG_DEBUG(DB::Logger::get(), "!!!!! read befored1.2! {} {} {} location.size_in_file {} remote_name={}", location.offset_in_file, file->getPos(), file->getPrefetchedSize(), location.size_in_file, remote_name);
             ProfileEvents::increment(ProfileEvents::S3PageReaderNotReusedFile2, 1);
+        }
     #ifdef DBMS_PUBLIC_GTEST
         if (remote_name_view.isLockFile())
         {
